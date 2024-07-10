07ff090afea8e34a7ea5567eea6d2ea5b2775178
Ruben Lee <rubenjavierlee@gmail.com>
2024-07-11 00:38:22 +0100

    changed selection behaviour since it had some bugs

--- scenes/cardUi/card_ui.gd
+++ scenes/cardUi/card_ui.gd
@@ -6,9 +6,6 @@ signal card_played()
 
 @onready var color_2: ColorRect = $Color2
 @onready var color: ColorRect = $Color
-@onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
-@onready var drop_point_detector = $DropPointDetector
-@onready var targets: Array[Node] = []
 @onready var card_image = $CardImage
 
 var player: int
@@ -19,28 +16,12 @@ var card_cover: String
 var card_visible: bool = false
 
 func _ready() -> void:
-	card_state_machine.init(self)
 	show_card()
 	
-func _input(event: InputEvent) -> void:
-	card_state_machine.on_input(event)
-	
 func _on_gui_input(event: InputEvent) -> void:
-	card_state_machine.on_gui_input(event)
+	if event.is_action_pressed("left_mouse") and (color.visible or color_2.visible):
+		card_clicked()
 	
-func _on_mouse_entered() -> void:
-	card_state_machine.on_mouse_entered()
-	
-func _on_mouse_exited() -> void:
-	card_state_machine.on_mouse_exited()
-
-func _on_drop_point_detector_area_entered(area):
-	if not targets.has(area):
-		targets.append(area)
-
-func _on_drop_point_detector_area_exited(area):
-	targets.erase(area)
-
 func show_card():
 	if card_visible:
 		card_image.texture = load("res://Graphics/cards/" + card_cover + ".png")
@@ -60,6 +41,5 @@ func card_clicked():
 	if ui_layer:
 		self.reparent(ui_layer)
 	tween.tween_property(self, "global_position", newPosition , 0.75)
-	#tween.tween_property(self, "rotation", 0 , 0.5)
 	await tween.finished
 	self.card_played.emit()
